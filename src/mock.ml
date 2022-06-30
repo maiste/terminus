078931a41751b62db3df6fd3eb3b9f8bc03b721e
Etienne MARAIS <etienne@maiste.fr>
2022-06-30 14:44:32 +0200

    Fix Mock module

--- src/mock.ml
+++ src/mock.ml
@@ -6,11 +6,11 @@ type query =
 [@@deriving show]
 
 exception Wrong_url of string
-exception Wrong_token of string
+exception Wrong_header of string
 
 module Http_mock (M : sig
   val address : string
-  val check_headers : (string * string) list -> bool
+  val expected_headers: (string * string) list
   val expect : (query * string) list
 end) =
 struct
@@ -30,7 +30,14 @@ struct
 
   let mocks = Mocks.of_seq @@ List.to_seq @@ M.expect
   let address_length = String.length address
-  let check_headers headers = assert (M.check_headers headers)
+  let check_headers headers =
+      List.iter (fun  (field, value) -> 
+        match List.assoc_opt field headers with
+        | None -> raise (Wrong_header (Format.sprintf "No value for field %s" field))
+        | Some value' ->
+                if value <> value' then
+                raise (Wrong_header (Format.sprintf "Values mismatches %s <> %s for field %s" value value' field))
+      ) M.expected_headers
 
   let starts_with ~prefix str =
     let len = String.length prefix in
@@ -43,7 +50,7 @@ struct
 
   let find key =
     try Mocks.find key mocks
-    with Not_found -> failwith (Printf.sprintf "Not_found %s" "TODO")
+    with Not_found -> failwith (Printf.sprintf "Not_found %s" (show_query key))
 
   let compute ~headers ~url fn =
     check_headers headers;
