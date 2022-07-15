e713c5d2a5b23c4bbff3b63ad68e058aed4385b3
Etienne MARAIS <etienne@maiste.fr>
2022-07-15 17:01:00 +0200

    Update documentation and license

--- src/mock.mli
+++ src/mock.mli
@@ -1,3 +1,16 @@
+(*—————————————————————————————————————————————————————————————————————
+   Copyright (c) 2022-* Etienne Marais <etienne@maiste.fr>
+   Distributed under the MIT license. See terms at the end of this
+   file.
+  ————————————————————————————————————————————————————————————————————*)
+
+(** The query type defines the interaction with the API:
+
+    - [Get path]: tries to execute the GET method on a specific path in the API
+    - [Post (path, expected)] defines a POST method on a path and verify the
+      input with [expected].
+    - [Put (path, expected)] works the same way as [Post]
+    - [Delete path] executes a DELETE method on a specific path *)
 type query =
   | Get of string
   | Post of string * string
@@ -6,9 +19,14 @@ type query =
 [@@deriving show]
 
 exception Wrong_url of string
+(** Exception raised when you are trying to execute a request on a wrong url or
+    when urls mismatched. *)
+
 exception Wrong_header of string
+(** Exception raised when on field in an header mismatch. *)
 
 module type Mock_backend = S.Intf with type 'a io = 'a
+(** A Mock backend is an implementation of the {!S} module with a mock backend. *)
 
 val mock :
   address:string ->
@@ -16,3 +34,22 @@ val mock :
   expect:(query * string) list ->
   unit ->
   (module Mock_backend)
+(** [mock ~address ~expected_headers ~expect ()] creates a new mock backend with
+    some checks in it. The [address] is the endpoint for the API. the
+    [expected_headers] are the data that should be found at each request in the
+    header. [expect] is a list of {!query} with the answer expected when you
+    execute the request. *)
+
+(*——————————————————————————————————————————————————————————————————————
+   Copyright (c) 2022-* Etienne Marais <etienne@maiste.fr>
+   Permission to use, copy, modify, and/or distribute this software for
+   any purpose with or without fee is hereby granted, provided that the
+   above copyright notice and this permission notice appear in all copies.
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+   CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+  ————————————————————————————————————————————————————————————————————*)
--- src/s.ml
+++ src/s.ml
@@ -1,3 +1,9 @@
+(*—————————————————————————————————————————————————————————————————————
+   Copyright (c) 2022-* Etienne Marais <etienne@maiste.fr>
+   Distributed under the MIT license. See terms at the end of this
+   file.
+  ————————————————————————————————————————————————————————————————————*)
+
 (** This module gathers all the methods you need to be able to execute HTTP
     requests to contact an API server. It must send application/json request. *)
 
@@ -9,9 +15,19 @@ module type Intf = sig
   (** The I/O monad used to execute HTTP request. *)
 
   val return : 'a -> 'a io
+  (** [return x] creates a value in the {!io} monad. *)
+
   val map : ('a -> 'b) -> 'a io -> 'b io
+  (** [map f x] executes the [f] function and then wrap the result in the {!io}
+      monad. *)
+
   val bind : ('a -> 'b io) -> 'a io -> 'b io
+  (** [bind f x] is the same as {!map} but the function [f] should handle the
+      binding into the {!io} monad. *)
+
   val fail : error -> 'a io
+  (** [fail err] is the equivalent of {!return} but with a failure in the {!io}
+      monad. *)
 
   val get : headers:headers -> url:string -> string io
   (** [get ~headers ~url] executes a request to the server as a [GET] call and,
@@ -31,3 +47,17 @@ module type Intf = sig
   (** [delete ~headers ~url] executes a request to the server as a [DELETE] call
       and returns the result as a {!string}. *)
 end
+
+(*——————————————————————————————————————————————————————————————————————
+   Copyright (c) 2022-* Etienne Marais <etienne@maiste.fr>
+   Permission to use, copy, modify, and/or distribute this software for
+   any purpose with or without fee is hereby granted, provided that the
+   above copyright notice and this permission notice appear in all copies.
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+   CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+  ————————————————————————————————————————————————————————————————————*)
